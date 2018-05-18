de63f10b145af6e3647cb30677543d52e4fd09e5
nuclearnic <nuclearnicdev@gmail.com>
2018-05-18 14:42:33 +0200

    initial commit

---
+++ new_make_iam_users.rb
@@ -0,0 +1,34 @@
+require 'aws-sdk'
+require 'securerandom'
+require 'pry'
+
+client = Aws::IAM::Client.new(
+  region: 'us-east-2',
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY']
+)
+
+# insert github user names here
+new_users = %w[manguy_1 manguy_2]
+
+new_users.each do |new_user|
+  password = SecureRandom.hex
+
+  user_resp = client.create_user({
+    user_name: new_user,
+  }).to_h
+
+  client.create_login_profile({
+    password: password,
+    password_reset_required: true,
+    user_name: user_resp[:user][:user_name]
+  })
+
+  client.add_user_to_group({
+    group_name: 'makers',
+    user_name: user_resp[:user][:user_name]
+  })
+
+  puts 'username: ' + user_resp[:user][:user_name]
+  puts 'password: ' + password
+end
